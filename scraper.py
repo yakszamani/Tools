afed7f58f9f74d9bd35dee37829b3ef9de43a0ae
Olasunkanmi(Yaki) <105818851+yakszamani@users.noreply.github.com>
2024-09-19 16:35:22 -0400

    Update scraper.py
    Added the csv functionality

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 page_scraped = requests.get("https://top100.indianapolis.iu.edu/about/past-recipients/#past", verify=False)
 
@@ -7,7 +8,14 @@ soup = BeautifulSoup(page_scraped.text, "html.parser")
 
 names = soup.find_all("tr")
 
+file = open("top100.csv", "w")
+writer = csv.writer(file)
+
+writer.writerow(["Name", "School"])
+
 for name in names:
     tds = name.find_all("td")
     td_texts = [td.get_text(strip=True) for td in tds]
     print(" - ".join(td_texts))
+    writer.writerow(td_texts)
+file.close()
